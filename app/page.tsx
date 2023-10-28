4bffef6798d6ca4197a28f43fba61e659c90305d
Yash Gupta <gyash21@gmail.com>
2023-10-28 13:35:30 +0530

    update- adding the validation function

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from "@/components//ui/form"
 import { useForm } from "react-hook-form"
+import * as z from "zod";
 
 
 
 export default function Home(){
-  const form = useForm({
+
+  const formSchema = z.object({
+
+     title: z.string()
+     .min(5, {message: "Hey the title is not long enough"})
+     .max(100, {message: "Its too long"}),
+     price: z.number().min(5, {message: "Enter anumber"}),
+     description: z.string().min(5, {message: "Hey the title is not long enough"})
+     .max(100, {message: "Its too long"})
+     .trim()
+
+    
+    })
+
+
+
+
+  const form = useForm<z.infer<typeof formSchema>>({
     mode: 'onChange',
     defaultValues:{
       title: '',
